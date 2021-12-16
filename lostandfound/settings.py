918a24a42b5506d9083af2d1dea2dc360c5a0895
ahorupa <38441297+ahorupa@users.noreply.github.com>
2021-12-16 08:37:57 +0100

    added all

--- lostandfound/settings.py
+++ lostandfound/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3b4#zy3=gp9@5tc6az7=%b4(j8tt@7^qnnf+y13q_cz)9d$2u@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
